d76c2e9dc96e926da124ecd1bd9479c2fb44f65a
NeoGirin <rhdqud89@gmail.com>
2021-12-17 13:01:31 +0900

    sql connect

--- dsExam/Controllers/HomeController.cs
+++ dsExam/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using dsExam.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using MySqlConnector;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
@@ -25,7 +27,68 @@ namespace dsExam.Controllers
 
         public IActionResult TicketList()
         {
+            var dt = new DataTable();
+            using (var conn = new MySqlConnection("Server=127.0.0.1;Port=3306;Database=myweb;Uid=root;Pwd=123;"))
+            {
+                conn.Open();
+                using (var cmd = new MySqlCommand())
+                {
+                    string status = "In Progress";
+                    cmd.Connection = conn;
+                    cmd.CommandText = @"
+SELECT
+	A.ticket_id
+,	A.title
+,	A.status
+FROM
+	t_ticket A
+WHERE 
+	A.status = @status
+";
+                    cmd.Parameters.AddWithValue("status", status);
+                    var reader = cmd.ExecuteReader();
+                    dt.Load(reader);
+
+                    //cmd.ExecuteNonQuery();
+                }
+            }
+            var list = new List<TicketModel>();
+            foreach(DataRow row in dt.Rows)
+            {
+                var ticket = new TicketModel();
+                ticket.Ticket_id = Convert.ToInt32(row["ticket_id"]);
+                ticket.Title = row["title"] as string;
+                ticket.Status = row["status"] as string;
+                list.Add(ticket);
+            }
+            ViewData["ticketList"] = list;
+            ViewData["dt"] = dt;
             return View();
+        }
+
+        public IActionResult TicketChange(int ticket_id, string title)
+        {
+            using (var conn = new MySqlConnection("Server=127.0.0.1;Port=3306;Database=myweb;Uid=root;Pwd=123;"))
+            {
+                conn.Open();
+                using (var cmd = new MySqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandText = @"
+UPDATE t_ticket
+SET
+	title = @title
+WHERE 
+	ticket_id = @ticket_id
+";
+                    cmd.Parameters.AddWithValue("ticket_id", ticket_id);
+                    cmd.Parameters.AddWithValue("@title", title);
+                    cmd.ExecuteNonQuery();
+                    //cmd.ExecuteNonQuery();
+                }
+            }
+            //return Json(new { msg = "OK" });
+            return Redirect("/home/ticketList");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
