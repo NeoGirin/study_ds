2ff35fa7591c40f1a5a189a84482954ff7442200
NeoGirin <rhdqud89@gmail.com>
2021-12-17 13:02:29 +0900

    add file

---
+++ dsExam/Models/TicketModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dsExam.Models
+{
+    public class TicketModel
+    {
+        public int Ticket_id { get; set; }
+        public string Title { get; set; }
+        public string Status { get; set; }
+    }
+}
