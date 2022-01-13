2b4884844339de6601798dc267c0df4a16c4d34b
NeoGirin <rhdqud89@gmail.com>
2022-01-13 10:32:59 +0900

    register, login, logout complete

---
+++ dsExam/Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dsExam.Controllers
+{
+    [Authorize]
+    public class AdminController : Controller
+    {
+        public AdminController()
+        {
+        }
+
+        [Authorize(Roles = "ADMIN")]
+        public IActionResult GetCheck()
+        {
+            return Json(new { a = 9 });
+        }
+
+        [AllowAnonymous] // 로그인 되지 않은 익명 사용자를 받겠다.
+        public IActionResult GetUserCheck()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return Json(new { a = 9 });
+            }
+            return Json(new { a = 1 });
+        }
+    }
+}
---
+++ dsExam/Services/CustomCookieAuthenticationEvents.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+public class CustomCookieAuthenticationEvents : CookieAuthenticationEvents
+{
+    public CustomCookieAuthenticationEvents()
+    {
+    }
+
+    public override async Task ValidatePrincipal(CookieValidatePrincipalContext context)
+    {
+        var userPrincipal = context.Principal;
+
+        var checkClaim = userPrincipal.Claims.First(p => p.Type == "LastCheckDateTime");
+        var lastCheckDateTime = DateTime.ParseExact(
+            userPrincipal.Claims.First(p => p.Type == "LastCheckDateTime").Value,
+            "yyyyMMddHHmmss", CultureInfo.InvariantCulture);
+        int intervalMin = 60;
+
+        if (lastCheckDateTime.AddSeconds(intervalMin) < DateTime.UtcNow)
+        {
+            // 이 사용자가 정상 사용자인지 검증
+            if (1 == 1)
+            {
+                var identity = userPrincipal.Identity as ClaimsIdentity;
+                identity.RemoveClaim(checkClaim);
+                identity.AddClaim(new Claim("LastCheckDateTime", DateTime.UtcNow.ToString("yyyyMMddHHmmss")));
+
+                await context.HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, userPrincipal);
+            }
+            else
+            {
+                context.RejectPrincipal();
+                await context.HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            }
+
+        }
+    }
+}
